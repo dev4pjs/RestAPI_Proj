663e1a3cbc832308b93458e812181178c9af72ea
devpjs <dev4pjs@gmail.com>
2015-02-11 18:47:47 +0530

    Updates

--- RestSamples/src/main/java/com/test/myrest/V5_FileProcessor.java
+++ RestSamples/src/main/java/com/test/myrest/V5_FileProcessor.java
@@ -1,5 +1,7 @@
 package com.test.myrest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -22,8 +24,33 @@ public class V5_FileProcessor {
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
     @Produces(MediaType.APPLICATION_JSON)
     public String uploadFile(InputStream uploadedInputStream) {
-        String resturnStr = uploadedInputStream.toString();
+        String resturnStr = "Hello World";
  
+        //FileInputStream fileinputstream = (FileInputStream)uploadedInputStream;
+		/*int content;
+		StringBuilder sb = new StringBuilder();
+		try {
+			while ((content = fileinputstream.read()) != -1) {
+				// convert to char and display it
+				System.out.print((char) content);
+				//resturnStr = resturnStr+((char)content);
+				sb.append((char)content);
+			}
+			resturnStr = resturnStr+sb.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Failed to Read the InputStream");
+			resturnStr = "Failed to read the InputStream";
+			e.printStackTrace();
+		}finally{
+			if(fileinputstream!=null)
+				try {
+					fileinputstream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}*/
 
         return resturnStr;
 
